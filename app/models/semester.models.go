f7a36823b18cc8bd695bf195320d7d4226d96b58
iqbal <emixbal@gmail.com>
2023-01-23 13:17:01 +0700

    perpendek

--- app/models/semester.models.go
+++ app/models/semester.models.go
@@ -2,7 +2,7 @@ package models
 
 type Semester struct {
 	MyGorm
-	Name           string       `json:"name" gorm:"size:50"`
+	Name           string       `json:"name" gorm:"size:1"`
 	AcademicYear   AcademicYear `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	AcademicYearID int          `json:"academic_year_id"`
 }
