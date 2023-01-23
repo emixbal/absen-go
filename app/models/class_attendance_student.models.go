7c103f0310ecf3db0b1108faab44602e33be3cfb
iqbal <emixbal@gmail.com>
2023-01-23 17:05:55 +0700

    simplify

--- app/models/class_attendance_student.models.go
+++ app/models/class_attendance_student.models.go
@@ -5,11 +5,11 @@ import (
 )
 
 type ClassAttendanceStudent struct {
-	MyGorm
-	Datang            time.Time       `json:"datang" `
-	Pulang            time.Time       `json:"pulang" `
+	ID                uint            `json:"id" gorm:"primarykey"`
 	ClassAttendance   ClassAttendance `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	ClassAttendanceID int             `json:"class_attendance_id"`
 	Student           Student         `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	StudentID         int             `json:"student_id"`
+	Datang            time.Time       `json:"datang" `
+	Pulang            time.Time       `json:"pulang" `
 }
--- app/models/extracurricular_attendance_student.models.go
+++ app/models/extracurricular_attendance_student.models.go
@@ -1,15 +1,13 @@
 package models
 
-import (
-	"time"
-)
+import "time"
 
 type ExtracurricularAttendanceStudent struct {
-	MyGorm
-	Datang                      time.Time                 `json:"datang" `
-	Pulang                      time.Time                 `json:"pulang" `
+	ID                          uint                      `json:"id" gorm:"primarykey"`
 	ExtracurricularAttendance   ExtracurricularAttendance `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	ExtracurricularAttendanceID int                       `json:"extracurricular_attendance_id"`
 	Student                     Student                   `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	StudentID                   int                       `json:"student_id"`
+	Datang                      time.Time                 `json:"datang" `
+	Pulang                      time.Time                 `json:"pulang" `
 }
