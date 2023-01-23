563ee9446ede99dbec93b3db4e8883863fdceab7
iqbal <emixbal@gmail.com>
2023-01-23 17:09:10 +0700

    add json parse

--- app/models/my_gorm.models.go
+++ app/models/my_gorm.models.go
@@ -10,5 +10,5 @@ type MyGorm struct {
 	ID        uint           `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
