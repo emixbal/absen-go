067fb326f08f4cdae2bc0d4f941c4d811f9d74b0
iqbal <emixbal@gmail.com>
2023-02-02 14:48:12 +0700

    ensure login in class-attendance

--- app/routers/class_attendance.rouetrs.go
+++ app/routers/class_attendance.rouetrs.go
@@ -2,12 +2,13 @@ package routers
 
 import (
 	"absen-go/app/controllers"
+	"absen-go/app/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func ClassAttendance(app *fiber.App) {
-	r := app.Group("/class-attendance")
+	r := app.Group("/class-attendance", middlewares.IsAuthenticated)
 
 	r.Post("/arrive", controllers.ClassAttendanceStudentArrive)
 	r.Post("/leave", controllers.ClassAttendanceStudentLeave)
