182b99107a5087b61abb89871311dc43c68875db
iqbal <emixbal@gmail.com>
2023-02-06 18:17:11 +0700

    fetchind data

--- app/models/recap_student_attendance.models.go
+++ app/models/recap_student_attendance.models.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"absen-go/config"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -11,30 +10,25 @@ import (
 )
 
 func RecapStudentAttendance(class_id, month string) Response {
+	type StudentAttendance struct {
+		AttendanceID int       `json:"attendance_id"`
+		Arrive       time.Time `json:"arrive"`
+		Leave        time.Time `json:"leave"`
+	}
 	type StudentResult struct {
-		ID   string `json:"id"`
-		Name string `json:"name"`
-		NISN string `json:"nisn"`
+		ID          string `json:"id"`
+		Name        string `json:"name"`
+		NISN        string `json:"nisn"`
+		Code        string `json:"code"`
+		Attendances []StudentAttendance
 	}
 
 	var res Response
-	var class_attendances []ClassAttendance
+	var sudent_result StudentResult
 	var sudents_result []StudentResult
+	var class_attendances []ClassAttendance
+
 	db := config.GetDBInstance()
-
-	if result := db.Table("students").
-		Select("students.id, students.name, students.nisn, students.code").
-		Where("students.class_id = ?", class_id).
-		Order("students.name asc").
-		Scan(&sudents_result); result.Error != nil {
-
-		log.Println("Err fetching data")
-		log.Println(result.Error)
-
-		res.Status = http.StatusInternalServerError
-		res.Message = "Err fetching students data"
-		return res
-	}
 
 	start_month, _ := time.Parse("2006-01-02", "2023-"+month+"-01")
 	end_month, _ := time.Parse("2006-01-02", "2023-"+month+"-28")
@@ -51,16 +45,59 @@ func RecapStudentAttendance(class_id, month string) Response {
 	if len(class_attendances) < 1 {
 		res.Status = http.StatusOK
 		res.Message = "tidak ada hari kerja pada bulan " + month
+		return res
 	}
 
-	fmt.Println(class_attendances)
+	rows_fetch_student, fetch_student_err := db.Table("students").
+		Select("students.id, students.name, students.nisn, students.code").
+		Where("students.class_id = ?", class_id).
+		Order("students.name asc").Rows()
+
+	defer rows_fetch_student.Close()
+
+	if fetch_student_err != nil {
+		log.Println("Err fetching data")
+		log.Println(fetch_student_err)
+
+		res.Status = http.StatusInternalServerError
+		res.Message = "Err fetching students data"
+		return res
+	}
+
+	for rows_fetch_student.Next() {
+		if err := rows_fetch_student.Scan(&sudent_result.ID, &sudent_result.Name, &sudent_result.NISN, &sudent_result.Code); err != nil {
+			res.Status = http.StatusInternalServerError
+			res.Message = "Err scan students"
+			return res
+		}
+
+		var cases []ClassAttendanceStudent
+		for _, class_attendance := range class_attendances {
+			var cas ClassAttendanceStudent
+			db.Where("class_attendance_id = ?", class_attendance.ID).Where("student_id = ?", sudent_result.ID).Take(&cas)
+			cases = append(cases, cas)
+		}
+
+		var arr_cas []StudentAttendance
+		for _, val := range cases {
+			var cas StudentAttendance
+
+			cas.AttendanceID = val.ClassAttendanceID
+			cas.Arrive = val.Arrive
+			cas.Leave = val.Leave
+
+			arr_cas = append(arr_cas, cas)
+		}
+
+		sudent_result.Attendances = arr_cas
+
+		sudents_result = append(sudents_result, sudent_result)
+	}
 
 	res.Status = http.StatusOK
 	res.Message = "ok"
 	res.Data = fiber.Map{
-		"student_in_class":  sudents_result,
-		"class_attendances": class_attendances,
+		"student_in_class": sudents_result,
 	}
-
 	return res
 }
