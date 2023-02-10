e65a0fb1174e522c22d138c806bc08844683cbd2
iqbal <emixbal@gmail.com>
2023-02-10 16:14:38 +0700

    add nis column

--- app/models/student.models.go
+++ app/models/student.models.go
@@ -11,6 +11,7 @@ type Student struct {
 	MyGorm
 	Name     string `json:"name" gorm:"size:100"`
 	NISN     string `json:"nisn" gorm:"size:10"`
+	NIS      string `json:"nis" gorm:"size:10"`
 	Code     string `json:"code" gorm:"size:20"`
 	Class    Class  `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 	ClassID  int    `json:"class_id"`
