b786499f3e49e722e9b3f02c0482ecb5477ea633
iqbal <emixbal@gmail.com>
2023-02-10 18:36:31 +0700

    date(3) return err

--- app/models/class_attendance.models.go
+++ app/models/class_attendance.models.go
@@ -1,16 +1,14 @@
 package models
 
-import (
-	"time"
-)
+import "time"
 
 type ClassAttendance struct {
 	MyGorm
-	Date       time.Time `json:"date" gorm:"type:date(3)"`
+	Date       time.Time `json:"date" gorm:"type:date"`
 	Class      Class     `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
-	ClassID    uint      `json:"class_id"`
+	ClassID    int       `json:"class_id"`
 	Semester   Semester  `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
-	SemesterID uint      `json:"semester_id"`
+	SemesterID int       `json:"semester_id"`
 	StartTime  string    `gorm:"type:time(3)" json:"start_time,omitempty"`
 	EndTime    string    `gorm:"type:time(3)" json:"end_time,omitempty"`
 }
