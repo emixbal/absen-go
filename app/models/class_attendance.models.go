a3e8969eacfe52d4d752b7dedfdc8e22de8458b2
iqbal <emixbal@gmail.com>
2023-02-16 12:58:39 +0700

    init: removing

--- app/models/class_attendance.models.go
+++ app/models/class_attendance.models.go
@@ -1,14 +1,12 @@
 package models
 
-import "time"
-
-type ClassAttendance struct {
-	MyGorm
-	Date       time.Time `json:"date" gorm:"type:date"`
-	Class      Class     `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
-	ClassID    int       `json:"class_id"`
-	Semester   Semester  `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
-	SemesterID int       `json:"semester_id"`
-	StartTime  string    `gorm:"type:time(3)" json:"start_time,omitempty"`
-	EndTime    string    `gorm:"type:time(3)" json:"end_time,omitempty"`
-}
+// type ClassAttendance struct {
+// 	MyGorm
+// 	Date       time.Time `json:"date" gorm:"type:date"`
+// 	Class      Class     `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
+// 	ClassID    int       `json:"class_id"`
+// 	Semester   Semester  `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
+// 	SemesterID int       `json:"semester_id"`
+// 	StartTime  string    `gorm:"type:time(3)" json:"start_time,omitempty"`
+// 	EndTime    string    `gorm:"type:time(3)" json:"end_time,omitempty"`
+// }
